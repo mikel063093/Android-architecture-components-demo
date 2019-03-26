0582580baee9bd3c3b282e032884af44a1226644
Miguel Alegria <mikel063093@gmail.com>
2019-03-26 03:26:32 -0500

    refactor fix

--- app/src/main/java/co/mike/zemoga/base/ViewModel.kt
+++ app/src/main/java/co/mike/zemoga/base/ViewModel.kt
@@ -5,21 +5,12 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import co.mike.zemoga.extencion.disposeWithoutFear
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
 
 abstract class ViewModel : BaseObservable(), LifecycleObserver {
 
     protected open val disposable = CompositeDisposable()
 
-    val showProgress: PublishSubject<Boolean> = PublishSubject.create<Boolean>()
-    val showMessage: PublishSubject<Int> = PublishSubject.create<Int>()
-    val showMessageText: PublishSubject<String> = PublishSubject.create<String>()
-    val showError: PublishSubject<Int> = PublishSubject.create<Int>()
-    val showErrorMessage: PublishSubject<String> = PublishSubject.create<String>()
-    val close: PublishSubject<Unit> = PublishSubject.create<Unit>()
 
     protected open fun subscribeToObservables() {}
 
@@ -46,27 +37,4 @@ abstract class ViewModel : BaseObservable(), LifecycleObserver {
         disposable.disposeWithoutFear()
     }
 
-    fun observableShowProgress(): Observable<Boolean> {
-        return showProgress.observeOn(AndroidSchedulers.mainThread())
-    }
-
-    fun observableShowMessage(): Observable<Int> {
-        return showMessage.observeOn(AndroidSchedulers.mainThread())
-    }
-
-    fun observableShowMessageText(): Observable<String> {
-        return showMessageText.observeOn(AndroidSchedulers.mainThread())
-    }
-
-    fun observableShowError(): Observable<Int> {
-        return showError.observeOn(AndroidSchedulers.mainThread())
-    }
-
-    fun observableShowErrorMessage(): Observable<String> {
-        return showErrorMessage.observeOn(AndroidSchedulers.mainThread())
-    }
-
-    fun observableClose(): Observable<Unit> {
-        return close.observeOn(AndroidSchedulers.mainThread())
-    }
 }
